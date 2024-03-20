fb0d18558ce5318ec9ea2bda7684bd93e8208ab5
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-03-20 18:07:02 +0500

    Add files via upload

---
+++ project calculator/index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let input = await inquirer.prompt([
+    {
+    message: "select your operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponent", "Modulus", "Percentage", "Percent"]},
+{ 
+    message:"Enter the first number",
+    type: "number",
+    name: "firstNumber"
+},
+{
+    message:"Enter the second number",
+    type: "number",
+    name: "SecondNumber"
+}
+]);
+
+if(input.operator === "Addition"){
+    console.log(input.firstNumber + input.SecondNumber);
+}else if(input.operator === "Subtraction"){
+    console.log(input.firstNumber - input.SecondNumber);
+}else if(input.operator === "Multiplication"){
+    console.log(input.firstNumber * input.SecondNumber);
+}else if(input.operator === "Division"){
+    console.log(input.firstNumber / input.SecondNumber);
+}else if(input.operator === "Exponent"){
+    console.log(input.firstNumber ** input.SecondNumber);
+}else if(input.operator === "Modulus"){
+    console.log(input.firstNumber % input.SecondNumber);
+}else if(input.operator === "Percentage"){
+    console.log(input.firstNumber / input.SecondNumber * 100);
+}else if(input.operator === "Percent"){
+    console.log(input.firstNumber * input.SecondNumber / 100);
+}else{
+    console.log("invalid operation");
+    
+}
